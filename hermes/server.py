4ffd45da56fcfc67a6d675cd84a735e5aebec3ed
Patrick Wang <patrick@covar.com>
2019-07-30 10:19:33 -0400

    use importlib.resources instead of pkg_resources

--- hermes/server.py
+++ hermes/server.py
@@ -1,6 +1,6 @@
 """hermes server."""
 import logging.config
-import pkg_resources
+import importlib.resources as pkg_resources
 import yaml
 
 from aiohttp import web
@@ -13,7 +13,7 @@ logger = logging.getLogger('hermes')
 def setup():
     """Set up app and logger."""
     # Set up default logger.
-    with open(pkg_resources.resource_filename('hermes', 'logging.yaml'), 'r') as f:
+    with pkg_resources.open_text('hermes', 'logging.yaml') as f:
         config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
 
