02e418bf366d312949ef142ce159369aacb6ea0f
Patrick Wang <patrick@covar.com>
2019-07-30 10:57:24 -0400

    unify local and Travis testing with tox

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""Setup file for hermes package."""
+from setuptools import setup
+
+setup(
+    name='hermes',
+    version='0.1.0-dev',
+    author='Patrick Wang',
+    author_email='patrick@covar.com',
+    url='https://github.com/NCATS-Gamma/hermes',
+    description='Pipelining for NCATS Translator services',
+    packages=['hermes'],
+    package_data={'hermes': ['logging.yaml']},
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT',
+    python_requires='>=3.7',
+)
