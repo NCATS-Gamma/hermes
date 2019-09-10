e0d179b96f8cdf7286bcbd53f5742e1f89d3e565
Patrick Wang <patrick@covar.com>
2019-09-10 12:03:45 -0400

    add CORS
    Apparently aiohttp_cors does not play nicely with pytest.

--- hermes/server.py
+++ hermes/server.py
@@ -1,9 +1,11 @@
 """hermes server."""
 import logging.config
 import importlib.resources as pkg_resources
+import os
 import yaml
 
 from aiohttp import web
+import aiohttp_cors
 
 from hermes.handlers import run, get_result, get_job
 
@@ -17,3 +19,17 @@ app = web.Application()
 app.router.add_post('/run', run)
 app.router.add_get('/result/{job_id}', get_result)
 app.router.add_get('/job/{job_id}', get_job)
+
+if os.environ.get('USE_CORS', False):
+    # Configure default CORS settings.
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+    })
+
+    # Configure CORS on all routes.
+    for route in list(app.router.routes()):
+        cors.add(route)
